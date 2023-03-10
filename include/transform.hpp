b0784f96ff6c39c3bc411fe934af4b276813b677
SamP-S <sam.ps1@outlook.com>
2023-03-10 19:53:40 +0000

    Add vec3 move, rotate, scale functions Add get direction functions

--- include/transform.hpp
+++ include/transform.hpp
@@ -1,4 +1,6 @@
-#include "la.hpp"
+#pragma once
+
+#include "la_extended.hpp"
 
 class Transform {
 
@@ -18,17 +20,46 @@ class Transform {
         void Translate(float x, float y, float z) {
             _position += vec3({x, y, z});
         }
+        void Translate(LA::vec3 translate) {
+            Translate(translate.x, translate.y, translate.z);
+        }
         // Rotation
         LA::vec3 GetRotation() { return _rotation; }
         void SetRotation(LA::vec3 rotation) { this->_rotation = rotation; }
         void Rotate(float x, float y, float z) {
             _rotation += vec3({x, y, z}); 
         }
+        void Rotate(LA::vec3 rotate) {
+            Rotate(rotate.x, rotate.y, rotate.z);
+        }
         // Scale
         LA::vec3 GetScale() { return _scale; }
         void SetScale(LA::vec3 scale) { this->_scale = scale; }
         void Scale(float x, float y, float z) {
            _scale += vec3({x, y, z});
+        }
+        void Scale(LA::vec3 scale) {
+            Scale(scale.x, scale.y, scale.z);
+        }
+
+        LA::mat4 GetTransform() {
+            return Transformation(_position, _rotation, _scale);
+        }
+
+        // negative the z as opengl looks down the z axis
+        LA::vec3 GetForward() {
+            mat4 total = GetTransform();
+            return vec3({ total[2][0], total[2][1], -total[2][2] });
+        }
+
+        LA::vec3 GetRight() {
+            mat4 total = GetTransform();
+            return vec3({ total[0][0], total[0][1], total[0][2] });
+        }
+
+        LA::vec3 GetUp() {
+            mat4 total = GetTransform();
+            return vec3({ total[1][0], total[1][1], total[1][2] });
         }
 
     private:

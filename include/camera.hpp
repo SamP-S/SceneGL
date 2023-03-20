25da4053e6724183449147df081fd4c9493e4119
Pearson-Smith, Sam <sam.pearson-smith@intel.com>
2023-03-20 22:08:07 +0000

    Add mouse control of camera

--- include/camera.hpp
+++ include/camera.hpp
@@ -18,31 +18,31 @@ class Camera {
         Transform trans = Transform();
 
         mat4 view = mat4();
+        mat4 view_inv = mat4();
         mat4 proj = mat4();
 
         float mouseSens = 0.3f;
         float speed = 0.01f;
         float FOV = 70.0f;
 
-        float horizontalAngle = 0.0f;
-        float verticalAngle = 0.0f;
+        bool active = false;
+
+        int x = 0;
+        int y = 0;
 
         void SetProjection(float FOV, float window_width, float window_height, float plane_near, float plane_far)
         {
             proj = Perspective(FOV, window_width / window_height, plane_near, plane_far);
         }
 
-        void RotateCamera(double dx, double dy)
+        void RotateCamera(int dx, int dy)
         {
-            horizontalAngle += mouseSens * float(-dx);
-            verticalAngle += mouseSens * float(dy);
+            float dxf = mouseSens * float(-dx);
+            float dyf = mouseSens * float(dy);
 
-            trans.SetRotation(vec3({
-                cosf(verticalAngle) * sinf(horizontalAngle),
-                sinf(verticalAngle),
-                cosf(verticalAngle) * cosf(horizontalAngle)
-            }));
+            trans.Rotate(0.0f, dxf, dyf);
             view = trans.GetTransform();
+            view_inv = inverse(trans.GetTransform());
         }
 
         void Move(int dir)
@@ -50,42 +50,61 @@ class Camera {
             vec3 translate = vec3();
             switch (dir) {
                 case MOVE_LEFT:
-                    translate = trans.GetRight() * speed;
-                    break;
-                case MOVE_RIGHT:
                     translate = trans.GetRight() * -speed;
                     break;
-                case MOVE_UP:
-                    translate = trans.GetUp() * -speed;
+                case MOVE_RIGHT:
+                    translate = trans.GetRight() * speed;
                     break;
-                case MOVE_DOWN:
+                case MOVE_UP:
                     translate = trans.GetUp() * speed;
                     break;
+                case MOVE_DOWN:
+                    translate = trans.GetUp() * -speed;
+                    break;
                 case MOVE_FORWARD:
-                    translate = trans.GetForward() * -speed;
+                    translate = trans.GetForward() * speed;
                     break;
                 case MOVE_BACKWARD:
-                    translate = trans.GetForward() * speed;
+                    translate = trans.GetForward() * -speed;
                     break;
             };
             trans.Translate(translate);
             view = trans.GetTransform();
+            view_inv = inverse(trans.GetTransform());
+            std::cout << "view" << std::endl;
+            print(view);
+            std::cout << "view_inverse:" << std::endl;
+            print(view_inv);
         }
 
         void Update()
         {
-            if (Input::GetKeyState("w"))
-                Move(MOVE_FORWARD);
-            if (Input::GetKeyState("s"))
-                Move(MOVE_BACKWARD);
-            if (Input::GetKeyState("a"))
-                Move(MOVE_LEFT);
-            if (Input::GetKeyState("d"))
-                Move(MOVE_RIGHT);
-            if (Input::GetKeyState("left ctrl"))
-                Move(MOVE_DOWN);
-            if (Input::GetKeyState("space"))
-                Move(MOVE_UP);
+            if (Input::GetMouseButtonState(2)) {
+                if (Input::GetKeyState("w"))
+                    Move(MOVE_FORWARD);
+                if (Input::GetKeyState("s"))
+                    Move(MOVE_BACKWARD);
+                if (Input::GetKeyState("a"))
+                    Move(MOVE_LEFT);
+                if (Input::GetKeyState("d"))
+                    Move(MOVE_RIGHT);
+                if (Input::GetKeyState("left ctrl"))
+                    Move(MOVE_DOWN);
+                if (Input::GetKeyState("space"))
+                    Move(MOVE_UP);
+
+                if (!active) {
+                    active = true;
+                } else {
+                    int dx = Input::GetMouseX() - x;
+                    int dy = Input::GetMouseY() - y;
+                    // RotateCamera(dx, dy);
+                }
+                x = Input::GetMouseX();
+                y = Input::GetMouseY();
+            } else {
+                active = false;
+            }
         }
 
 };

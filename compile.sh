a4fc247c70b721871b213372fb62b5789f0666ef
Pearson-Smith, Sam <sam.pearson-smith@intel.com>
2023-03-20 15:21:53 +0000

    Change compile to cmake from g++

--- compile.sh
+++ compile.sh
@@ -1,23 +1,27 @@
-g++ src/main.cpp \
-    dependencies/imgui/*.cpp \
-    dependencies/imgui/backends/imgui_impl_sdl.cpp \
-    dependencies/imgui/backends/imgui_impl_opengl3.cpp \
-    dependencies/tinyfiledialogs/tinyfiledialogs.c \
-    -o \
-    bin/linux/graphics \
-    -std=c++17 \
-    -w \
-    -Wfatal-errors \
-    -Idependencies/imgui \
-    -Idependencies/tinyfiledialogs \
-    -Idependencies/stb-master \
-    -I/usr/include/SDL2 \
-    -Iinclude \
-    -Llib \
-    -lGL \
-    -lGLU \
-    -lGLEW \
-    -lSDL2 \
-    -g \
-    -D LA_OPEN_GL
-    
+# g++ src/main.cpp \
+#     dependencies/imgui/*.cpp \
+#     dependencies/imgui/backends/imgui_impl_sdl.cpp \
+#     dependencies/imgui/backends/imgui_impl_opengl3.cpp \
+#     dependencies/tinyfiledialogs/tinyfiledialogs.c \
+#     -o \
+#     bin/linux/graphics \
+#     -std=c++17 \
+#     -w \
+#     -Wfatal-errors \
+#     -Idependencies/imgui \
+#     -Idependencies/tinyfiledialogs \
+#     -Idependencies/stb-master \
+#     -I/usr/include/SDL2 \
+#     -Iinclude \
+#     -Llib \
+#     -lGL \
+#     -lGLU \
+#     -lGLEW \
+#     -lSDL2 \
+#     -g \
+#     -D LA_OPEN_GL
+if [[ "$1" == "clean" ]]; then
+    rm -rf build
+fi
+cmake -S . -B build
+cmake --build build

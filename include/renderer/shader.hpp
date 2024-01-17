a1bdf2982af4b90173a228de148731b6ce9a98e7
SamP-S <sam.ps1@outlook.com>
2024-01-17 23:04:46 +0000

    Update shader to use shaderstage

--- include/renderer/shader.hpp
+++ include/renderer/shader.hpp
@@ -10,31 +10,32 @@
 #include <fstream>
 #include <sstream>
 
-#include "gl_interface.hpp"
 #include "ecs/resource.hpp"
 #include "ecs/resource_manager.hpp"
-
-#define SHADER_INVALID  0
-#define SHADER_VERTEX   1
-#define SHADER_FRAGMENT 2
-#define SHADER_COMPUTE  3
-#define SHADER_GEOMETRY 4
-#define SHADER_TESSELLATION_CONTROL     5
-#define SHADER_TESSELLATION_EVALUATION  6
+#include "renderer/shader_stage.hpp"
+#include "renderer/gl_interface.hpp"
 
 class Shader : public Resource {
-
     private:
+        ShaderStage* vs = new ShaderStage();
+        ShaderStage* fs = new ShaderStage();
         bool _validShader = false;
-        int _shaderType = SHADER_INVALID;
+        uint32_t _id = -1;
 
         bool Compile() {
-            const char* vShaderCode = vertexCode.c_str();
-            const char * fShaderCode = fragmentCode.c_str();
-            
+            if (vs == nullptr || fs == nullptr) {
+                std::cout << "WARNING (Shader): Can't compile shader with null stage." << std::endl;
+                return false;
+            }
+
+            if (vs->GetStage() != SHADER_VERTEX || fs->GetStage() != SHADER_FRAGMENT) {
+                std::cout << "WARNING (Shader): Trying to compile non matching stages." << std::endl;
+                return false;
+            }
+
             // shader Program
             try {
-                this->id = GL_Interface::CreateShaderProgram(vShaderCode, fShaderCode);
+                _id = GL_Interface::CreateShaderProgram(fs->GetSource().c_str(), vs->GetSource().c_str());
             } catch (const char* err) {
                 std::cout << "Error: Shader::Compile - Complilation failed";
                 std::cout << err << std::endl;
@@ -43,26 +44,13 @@ class Shader : public Resource {
             return true;
         }
 
-
     public:
-        unsigned int id;
-        std::string vertexCode = "";
-        std::string fragmentCode = "";
-
-        Shader(std::string name, std::string vs, std::string fs)
+        Shader(std::string name="Default Shader", ShaderStage* vs=nullptr, ShaderStage* fs=nullptr)
             : Resource(name) {
-            if (vs == "" || fs == "") {
-                throw "Shader Source Invalid";
-            }
-            this->vertexCode = vs;
-            this->fragmentCode = fs;
-            this->_validShader = Compile();
+            _validShader = Compile();
         }
 
-        Shader(std::string name, const char* vFilePath, const char* fFilePath)
-            : Shader(name, GetSourceFromFile(vFilePath), GetSourceFromFile(fFilePath)) {
-        }
-
+        // JSON
         std::string Type() {
             return "Shader";
         }
@@ -76,54 +64,55 @@ class Shader : public Resource {
         std::string ToString() {
             return "Shader";
         }
-
-
-        bool Use() {
-            return GL_Interface::UseShader(this->id);
-        }
-
-        bool isValid() {
+        
+        // validity getter
+        bool IsValid() {
             return _validShader;
         }
 
+        // OpenGL based calls
+        bool Use() {
+            return GL_Interface::UseShader(_id);
+        }
+
         void SetBool(const std::string& name, bool value) const {
-            glUniform1i(glGetUniformLocation(id, name.c_str()), value);
+            glUniform1i(glGetUniformLocation(_id, name.c_str()), value);
         }
 
         void SetInt(const std::string& name, int value) const {
-            glUniform1i(glGetUniformLocation(id, name.c_str()), value);
+            glUniform1i(glGetUniformLocation(_id, name.c_str()), value);
         }
 
         void SetFloat(const std::string& name, float value) const {
-            glUniform1f(glGetUniformLocation(id, name.c_str()), value);
+            glUniform1f(glGetUniformLocation(_id, name.c_str()), value);
         }
 
         void SetVec2(const std::string& name, vec2 v) const {
-            glUniform2f(glGetUniformLocation(id, name.c_str()), v.x, v.y);
+            glUniform2f(glGetUniformLocation(_id, name.c_str()), v.x, v.y);
         }
 
         void SetVec2(const std::string& name, float x, float y) const {
-            glUniform2f(glGetUniformLocation(id, name.c_str()), x, y);
+            glUniform2f(glGetUniformLocation(_id, name.c_str()), x, y);
         }
 
         void SetVec3(const std::string& name, vec3 v) const {
-            glUniform3f(glGetUniformLocation(id, name.c_str()), v.x, v.y, v.z);
+            glUniform3f(glGetUniformLocation(_id, name.c_str()), v.x, v.y, v.z);
         }
 
         void SetVec3(const std::string& name, float x, float y, float z) const {
-            glUniform3f(glGetUniformLocation(id, name.c_str()), x, y, z);
+            glUniform3f(glGetUniformLocation(_id, name.c_str()), x, y, z);
         }
 
         void SetVec4(const std::string& name, vec4 v) const {
-            glUniform4f(glGetUniformLocation(id, name.c_str()), v.x, v.y, v.z, v.w);
+            glUniform4f(glGetUniformLocation(_id, name.c_str()), v.x, v.y, v.z, v.w);
         }
         
         void SetVec4(const std::string& name, float x, float y, float z, float w) const {
-            glUniform4f(glGetUniformLocation(id, name.c_str()), x, y, z, w);
+            glUniform4f(glGetUniformLocation(_id, name.c_str()), x, y, z, w);
         }
 
         void SetMat4(const std::string& name, float* mPtr) const {
-            glUniformMatrix4fv(glGetUniformLocation(id, name.c_str()), 1, GL_FALSE, mPtr);
+            glUniformMatrix4fv(glGetUniformLocation(_id, name.c_str()), 1, GL_FALSE, mPtr);
         }
 
 };

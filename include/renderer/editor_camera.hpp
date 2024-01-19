92f0cfa827fef31a7b8fdbe807d3a8c7584a6faf
SamP-S <sam.ps1@outlook.com>
2024-01-19 04:24:51 +0000

    Impl proj to editor camera

--- include/renderer/editor_camera.hpp
+++ include/renderer/editor_camera.hpp
@@ -15,15 +15,27 @@
 
 class EditorCamera {
     public:
+        // camera properties
+        float fov = 45.0f;
+        float near = 0.1f;
+        float far = 100.0f;
+        uint32_t width = 800;
+        uint32_t height = 600;
+        // first person controller
         bool active = false;
         int x = 0;
         int y = 0;
         float sens = 5.0f;
         float speed = 5.0f;
+        // transform
         TransformComponent transform = TransformComponent();
         
         EditorCamera() {}
         ~EditorCamera() {}
+
+        LA::mat4 GetProjection() {
+            return LA::Perspective(fov, (float)width / (float)height, near, far);
+        }
 
         void RotateCamera(int dx, int dy)
         {

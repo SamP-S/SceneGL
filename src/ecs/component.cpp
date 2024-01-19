b200cc236665e48cac0f9a50ed136fa623ddfa5a
SamP-S <sam.ps1@outlook.com>
2024-01-19 03:04:51 +0000

    Delete all ECS. Break everything.

--- src/ecs/component.cpp
+++
@@ -1,8 +0,0 @@
-#include "ecs/component.hpp"
-#include "ecs/entity.hpp"
-
-Component::Component(Entity* entity) :
-    entity(entity),
-    transform(entity->transform) {}
-
-Component::~Component() {}

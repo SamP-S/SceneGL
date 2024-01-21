f60c5e911098ab4290b9efee455d88c7892b4ed4
SamP-S <sam.ps1@outlook.com>
2024-01-21 19:44:57 +0000

    Migrate imguizmo test to use LA

--- test/imguizmo_test.cpp
+++ test/imguizmo_test.cpp
@@ -32,10 +32,11 @@
 #include "backends/imgui_impl_sdl2.h"
 #include "backends/imgui_impl_opengl3.h"
 
+#include "la_extended.h"
 #include "ImGuizmo.h"
-#include <math.h>
 #include <vector>
 #include <algorithm>
+
 
 namespace ImApp {
    struct Config
@@ -119,165 +120,37 @@ int gizmoCount = 1;
 float camDistance = 8.f;
 static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
 
-float objectMatrix[4][16] = {
-  { 1.f, 0.f, 0.f, 0.f,
-    0.f, 1.f, 0.f, 0.f,
-    0.f, 0.f, 1.f, 0.f,
-    0.f, 0.f, 0.f, 1.f },
-
-  { 1.f, 0.f, 0.f, 0.f,
-  0.f, 1.f, 0.f, 0.f,
-  0.f, 0.f, 1.f, 0.f,
-  2.f, 0.f, 0.f, 1.f },
-
-  { 1.f, 0.f, 0.f, 0.f,
-  0.f, 1.f, 0.f, 0.f,
-  0.f, 0.f, 1.f, 0.f,
-  2.f, 0.f, 2.f, 1.f },
-
-  { 1.f, 0.f, 0.f, 0.f,
-  0.f, 1.f, 0.f, 0.f,
-  0.f, 0.f, 1.f, 0.f,
-  0.f, 0.f, 2.f, 1.f }
+LA::mat4 objectMatrix[4] = {
+   LA::mat4({
+      { 1.f, 0.f, 0.f, 0.f},
+      { 0.f, 1.f, 0.f, 0.f},
+      { 0.f, 0.f, 1.f, 0.f},
+      { 0.f, 0.f, 0.f, 1.f}
+   }),
+   LA::mat4({
+      { 1.f, 0.f, 0.f, 0.f},
+      { 0.f, 1.f, 0.f, 0.f},
+      { 0.f, 0.f, 1.f, 0.f},
+      { 2.f, 0.f, 0.f, 1.f}
+   }),
+   LA::mat4({
+      { 1.f, 0.f, 0.f, 0.f},
+      { 0.f, 1.f, 0.f, 0.f},
+      { 0.f, 0.f, 1.f, 0.f},
+      { 2.f, 0.f, 2.f, 1.f}
+   }),
+   LA::mat4({
+      { 1.f, 0.f, 0.f, 0.f},
+      { 0.f, 1.f, 0.f, 0.f},
+      { 0.f, 0.f, 1.f, 0.f},
+      { 0.f, 0.f, 2.f, 1.f}
+   })
 };
 
-static const float identityMatrix[16] =
-{ 1.f, 0.f, 0.f, 0.f,
-    0.f, 1.f, 0.f, 0.f,
-    0.f, 0.f, 1.f, 0.f,
-    0.f, 0.f, 0.f, 1.f };
+static const LA::mat4 identityMatrix = LA::mat4();
 
-void Frustum(float left, float right, float bottom, float top, float znear, float zfar, float* m16)
-{
-   float temp, temp2, temp3, temp4;
-   temp = 2.0f * znear;
-   temp2 = right - left;
-   temp3 = top - bottom;
-   temp4 = zfar - znear;
-   m16[0] = temp / temp2;
-   m16[1] = 0.0;
-   m16[2] = 0.0;
-   m16[3] = 0.0;
-   m16[4] = 0.0;
-   m16[5] = temp / temp3;
-   m16[6] = 0.0;
-   m16[7] = 0.0;
-   m16[8] = (right + left) / temp2;
-   m16[9] = (top + bottom) / temp3;
-   m16[10] = (-zfar - znear) / temp4;
-   m16[11] = -1.0f;
-   m16[12] = 0.0;
-   m16[13] = 0.0;
-   m16[14] = (-temp * zfar) / temp4;
-   m16[15] = 0.0;
-}
 
-void Perspective(float fovyInDegrees, float aspectRatio, float znear, float zfar, float* m16)
-{
-   float ymax, xmax;
-   ymax = znear * tanf(fovyInDegrees * 3.141592f / 180.0f);
-   xmax = ymax * aspectRatio;
-   Frustum(-xmax, xmax, -ymax, ymax, znear, zfar, m16);
-}
-
-void Cross(const float* a, const float* b, float* r)
-{
-   r[0] = a[1] * b[2] - a[2] * b[1];
-   r[1] = a[2] * b[0] - a[0] * b[2];
-   r[2] = a[0] * b[1] - a[1] * b[0];
-}
-
-float Dot(const float* a, const float* b)
-{
-   return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-}
-
-void Normalize(const float* a, float* r)
-{
-   float il = 1.f / (sqrtf(Dot(a, a)) + FLT_EPSILON);
-   r[0] = a[0] * il;
-   r[1] = a[1] * il;
-   r[2] = a[2] * il;
-}
-
-void LookAt(const float* eye, const float* at, const float* up, float* m16)
-{
-   float X[3], Y[3], Z[3], tmp[3];
-
-   tmp[0] = eye[0] - at[0];
-   tmp[1] = eye[1] - at[1];
-   tmp[2] = eye[2] - at[2];
-   Normalize(tmp, Z);
-   Normalize(up, Y);
-
-   Cross(Y, Z, tmp);
-   Normalize(tmp, X);
-
-   Cross(Z, X, tmp);
-   Normalize(tmp, Y);
-
-   m16[0] = X[0];
-   m16[1] = Y[0];
-   m16[2] = Z[0];
-   m16[3] = 0.0f;
-   m16[4] = X[1];
-   m16[5] = Y[1];
-   m16[6] = Z[1];
-   m16[7] = 0.0f;
-   m16[8] = X[2];
-   m16[9] = Y[2];
-   m16[10] = Z[2];
-   m16[11] = 0.0f;
-   m16[12] = -Dot(X, eye);
-   m16[13] = -Dot(Y, eye);
-   m16[14] = -Dot(Z, eye);
-   m16[15] = 1.0f;
-}
-
-void OrthoGraphic(const float l, float r, float b, const float t, float zn, const float zf, float* m16)
-{
-   m16[0] = 2 / (r - l);
-   m16[1] = 0.0f;
-   m16[2] = 0.0f;
-   m16[3] = 0.0f;
-   m16[4] = 0.0f;
-   m16[5] = 2 / (t - b);
-   m16[6] = 0.0f;
-   m16[7] = 0.0f;
-   m16[8] = 0.0f;
-   m16[9] = 0.0f;
-   m16[10] = 1.0f / (zf - zn);
-   m16[11] = 0.0f;
-   m16[12] = (l + r) / (l - r);
-   m16[13] = (t + b) / (b - t);
-   m16[14] = zn / (zn - zf);
-   m16[15] = 1.0f;
-}
-
-inline void rotationY(const float angle, float* m16)
-{
-   float c = cosf(angle);
-   float s = sinf(angle);
-
-   m16[0] = c;
-   m16[1] = 0.0f;
-   m16[2] = -s;
-   m16[3] = 0.0f;
-   m16[4] = 0.0f;
-   m16[5] = 1.f;
-   m16[6] = 0.0f;
-   m16[7] = 0.0f;
-   m16[8] = s;
-   m16[9] = 0.0f;
-   m16[10] = c;
-   m16[11] = 0.0f;
-   m16[12] = 0.f;
-   m16[13] = 0.f;
-   m16[14] = 0.f;
-   m16[15] = 1.0f;
-}
-
-void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bool editTransformDecomposition)
+void EditTransform(LA::mat4& cameraView, LA::mat4& cameraProjection, LA::mat4& matrix, bool editTransformDecomposition)
 {
    static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
    static bool useSnap = false;
@@ -306,11 +179,11 @@ void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bo
       if (ImGui::RadioButton("Universal", mCurrentGizmoOperation == ImGuizmo::UNIVERSAL))
          mCurrentGizmoOperation = ImGuizmo::UNIVERSAL;
       float matrixTranslation[3], matrixRotation[3], matrixScale[3];
-      ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation, matrixRotation, matrixScale);
+      ImGuizmo::DecomposeMatrixToComponents((float*)&matrix, matrixTranslation, matrixRotation, matrixScale);
       ImGui::InputFloat3("Tr", matrixTranslation);
       ImGui::InputFloat3("Rt", matrixRotation);
       ImGui::InputFloat3("Sc", matrixScale);
-      ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, matrix);
+      ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, (float*)&matrix);
 
       if (mCurrentGizmoOperation != ImGuizmo::SCALE)
       {
@@ -372,11 +245,11 @@ void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bo
       ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
    }
 
-   ImGuizmo::DrawGrid(cameraView, cameraProjection, identityMatrix, 100.f);
-   ImGuizmo::DrawCubes(cameraView, cameraProjection, &objectMatrix[0][0], gizmoCount);
-   ImGuizmo::Manipulate(cameraView, cameraProjection, mCurrentGizmoOperation, mCurrentGizmoMode, matrix, NULL, useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
+   ImGuizmo::DrawGrid((float*)&cameraView, (float*)&cameraProjection, (float*)&identityMatrix, 100.f);
+   ImGuizmo::DrawCubes((float*)&cameraView, (float*)&cameraProjection, (float*)&objectMatrix[0], gizmoCount);
+   ImGuizmo::Manipulate((float*)&cameraView, (float*)&cameraProjection, mCurrentGizmoOperation, mCurrentGizmoMode, (float*)&matrix, NULL, useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
 
-   ImGuizmo::ViewManipulate(cameraView, camDistance, ImVec2(viewManipulateRight - 128, viewManipulateTop), ImVec2(128, 128), 0x10101010);
+   ImGuizmo::ViewManipulate((float*)&cameraView, camDistance, ImVec2(viewManipulateRight - 128, viewManipulateTop), ImVec2(128, 128), 0x10101010);
 
    if (useWindow)
    {
@@ -397,13 +270,8 @@ int main(int, char**)
 
    int lastUsing = 0;
 
-   float cameraView[16] =
-   { 1.f, 0.f, 0.f, 0.f,
-     0.f, 1.f, 0.f, 0.f,
-     0.f, 0.f, 1.f, 0.f,
-     0.f, 0.f, 0.f, 1.f };
-
-   float cameraProjection[16];
+   LA::mat4 cameraView = LA::mat4();
+   LA::mat4 cameraProjection = LA::mat4();
 
    // Camera projection
    bool isPerspective = true;
@@ -422,12 +290,12 @@ int main(int, char**)
       ImGuiIO& io = ImGui::GetIO();
       if (isPerspective)
       {
-         Perspective(fov, io.DisplaySize.x / io.DisplaySize.y, 0.1f, 100.f, cameraProjection);
+         cameraProjection = LA::Perspective(fov, io.DisplaySize.x / io.DisplaySize.y, 0.1f, 100.f);
       }
       else
       {
          float viewHeight = viewWidth * io.DisplaySize.y / io.DisplaySize.x;
-         OrthoGraphic(-viewWidth, viewWidth, -viewHeight, viewHeight, 1000.f, -1000.f, cameraProjection);
+         cameraProjection = LA::Orthographic(-viewWidth, viewWidth, -viewHeight, viewHeight, 1000.f, -1000.f);
       }
       ImGuizmo::SetOrthographic(!isPerspective);
       ImGuizmo::BeginFrame();
@@ -461,10 +329,14 @@ int main(int, char**)
 
       if (viewDirty || firstFrame)
       {
-         float eye[] = { cosf(camYAngle) * cosf(camXAngle) * camDistance, sinf(camXAngle) * camDistance, sinf(camYAngle) * cosf(camXAngle) * camDistance };
-         float at[] = { 0.f, 0.f, 0.f };
-         float up[] = { 0.f, 1.f, 0.f };
-         LookAt(eye, at, up, cameraView);
+         LA::vec3 eye = LA::vec3({ 
+            cosf(camYAngle) * cosf(camXAngle) * camDistance,
+            sinf(camXAngle) * camDistance,
+            sinf(camYAngle) * cosf(camXAngle) * camDistance
+         });
+         LA::vec3 at = LA::vec3({ 0.f, 0.f, 0.f });
+         LA::vec3 up = LA::vec3({ 0.f, 1.f, 0.f });
+         cameraView = LA::LookAt(eye, at, up);
          firstFrame = false;
       }
 

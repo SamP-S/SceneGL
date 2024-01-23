1ab80be49ce3c6a92a8d10676e549e81604e12e4
SamP-S <sam.ps1@outlook.com>
2024-01-23 16:52:24 +0000

    Take systems as shared_ptr

--- include/gui/im_statistics.hpp
+++ include/gui/im_statistics.hpp
@@ -8,10 +8,10 @@
 
 class ImStatistics : IImWindow {
 public:
-    static void StastisticsWindow(bool* isOpen, TickTimer& tt) {
+    static void StastisticsWindow(bool* isOpen, std::shared_ptr<TickTimer> tt) {
         ImGui::Begin("Statistics Monitor", isOpen, _windowFlags);
         ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        ImGui::Text(("Tick Count: " + std::to_string(tt.GetTickCount())).c_str());
+        ImGui::Text(("Tick Count: " + std::to_string(tt->GetTickCount())).c_str());
         ImGui::End();
     }
 

d63a56fe2d92f2f92574c32d4e3314bf0a3d1711
SamP-S <sam.ps1@outlook.com>
2024-01-26 15:53:59 +0000

    Extract editor to spectator

--- sprinter/src/main.cpp
+++ sprinter/src/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include "editor.hpp"
 
 int main(int argc, char *argv[])
 {
     // instance main application
     std::cout << "start of program" << std::endl;
-    Editor app = Editor();// }
+    // Editor app = Editor();
+    std::cout << "sprinter not implemented" << std::endl;
     std::cout << "end of program" << std::endl;
     return 0;
 }

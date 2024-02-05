0237217d17a1dfcd2896600a2584be7eb58eb732
SamP-S <sam.ps1@outlook.com>
2024-02-05 22:33:29 +0000

    Rm state renderer base

--- marathon/include/renderer/renderer.hpp
+++ marathon/include/renderer/renderer.hpp
@@ -14,31 +14,13 @@
 #include "core/module.hpp"
 
 #include "la_extended.h"
+#include "renderer/renderer_api.hpp"
 #include "renderer/components.hpp"
 #include "renderer/shader.hpp"
 #include "renderer/frame_buffer.hpp"
 #include "renderer/material.hpp"
+#include "renderer/context.hpp"
 
-//// TODO:
-// Add depth test function
-
-//// NOTES:
-// The only state stored with the renderer currently:
-//      framebuffer
-//      view transform
-//      projection transform
-//      opengl context specific settings
-
-enum class DrawMode {
-    POINTS,
-    LINES,
-    FILL
-};
-
-enum class Winding {
-    CW,
-    CCW
-};
 
 class Renderer : public Module {
 protected:
@@ -46,15 +28,6 @@ protected:
     std::shared_ptr<FrameBuffer> _frameBuffer = nullptr;
     LA::mat4 _projection = LA::mat4();
     LA::mat4 _view = LA::mat4();
-
-    // opengl context settings
-    DrawMode _drawMode = DrawMode::FILL;
-    float _pointSize = 5.0f;
-    float _lineWidth = 3.0f;
-    bool _depthTesting = true;
-    bool _culling = true;
-    Winding _winding = Winding::CCW;
-    LA::vec4 _clearColour = LA::vec4({0.35f, 0.35f, 0.5f, 1.0f});
 
 public:
     std::string GetName() override {
@@ -64,20 +37,7 @@ public:
         return ModuleType::RENDER;
     }
 
-    virtual void SetDrawMode(DrawMode m) = 0;
-    virtual DrawMode GetDrawMode() = 0;
-    virtual void SetPointSize(float size) = 0;
-    virtual float GetPointSize() = 0;
-    virtual void SetLineWidth(float width) = 0;
-    virtual float GetLineWidth() = 0;
-    virtual void SetWinding(Winding winding) = 0;
-    virtual Winding GetWinding() = 0;
-    virtual void SetClearColour(const LA::vec4& colour) = 0;
-    virtual LA::vec4 GetClearColour() = 0;
-    virtual void SetCulling(bool enabled) = 0;
-    virtual bool IsCulling() = 0;
-    virtual void SetDepthTesting(bool enabled) = 0;
-    virtual bool IsDepthTesting() = 0;
+    std::shared_ptr<Context> context = nullptr;
 
     // set null to draw directly to screen
     virtual void SetFrameBuffer(std::shared_ptr<FrameBuffer> fb) = 0;
@@ -96,6 +56,8 @@ public:
     // delete copy and assign operators should always get instance from class::instance func
     Renderer(const Renderer&) = delete;
     Renderer& operator=(const Renderer&) = delete;
+
+    static Renderer& Instance();
         
 protected:
     Renderer() = default;
